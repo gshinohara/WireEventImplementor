d1875de48b15728801c86e476203f2eeda1f91ae
Gaku Shinohara <gaku.shinohara@vicc.jp>
2024-09-14 09:14:45 +0900

    Remove implementation of assembly priority

--- WireEventImplementor/WireUtil.cs
+++ WireEventImplementor/WireUtil.cs
@@ -14,14 +14,16 @@ namespace WireEventImplementor
 
     public static class WireUtil
     {
-        internal static void GetWireParameters(this GH_WireInteraction interaction, out IGH_Param source, out IGH_Param target, out LinkMode mode, out bool dragFromInput)
+        internal static void GetWireParameters(this GH_WireInteraction self, out WireStatus wireStatus)
         {
-            Func<string, object> getPrivateParam = (name) => typeof(GH_WireInteraction).GetField(name, BindingFlags.NonPublic | BindingFlags.Instance).GetValue(interaction);
+            Func<string, object> getPrivateParam = (name) => typeof(GH_WireInteraction).GetField(name, BindingFlags.NonPublic | BindingFlags.Instance).GetValue(self);
 
-            source = getPrivateParam("m_source") as IGH_Param;
-            target = getPrivateParam("m_target") as IGH_Param;
-            mode = (LinkMode)Enum.Parse(typeof(LinkMode), getPrivateParam("m_mode").ToString());
-            dragFromInput = (bool)getPrivateParam("m_dragfrominput");
+            IGH_Param source = getPrivateParam("m_source") as IGH_Param;
+            IGH_Param target = getPrivateParam("m_target") as IGH_Param;
+            LinkMode mode = (LinkMode)Enum.Parse(typeof(LinkMode), getPrivateParam("m_mode").ToString());
+            bool dragFromInput = (bool)getPrivateParam("m_dragfrominput");
+
+            wireStatus = new WireStatus(source, target, mode, dragFromInput);
         }
     }
 }
